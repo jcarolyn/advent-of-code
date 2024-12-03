4e0ebe090a00a50e8863bbdf6d56926a573a4b01
carolyn <carolyndoestech@gmail.com>
2024-12-02 18:14:42 -0600

    part 1 of day2 completed

--- 2024/day2/src/main.rs
+++ 2024/day2/src/main.rs
@@ -1,3 +1,56 @@
+use std::fs;
+use std::io::{self, BufRead};
+
+const FILE_PATH: &str = "input.txt";
+const MIN_LEVEL_DIFFERENCE: i32 = 1;
+const MAX_LEVEL_DIFFERENCE: i32 = 3;
+
+fn analyze_reports(file_path: &str) -> Result<i32, io::Error> {
+    let file = fs::File::open(file_path)?;
+    let reader = io::BufReader::new(file);
+
+    let mut total_safe_reports = 0;
+
+    for line in reader.lines() {
+        let line = line?;
+        // Split each line via whitespace
+        let levels: Vec<i32> = line.split_whitespace()
+            .map(|n| n.parse().unwrap())
+            .collect();
+
+        let mut increasing: bool = false;
+        let mut decreasing: bool = false;
+        let mut is_safe: bool = true;
+
+        // Windows allows you to create an iterator over a specific sized slice of vector
+        // in this case, the slice is size 2 so we can compare adjacent numbers
+        for window in levels.windows(2) {
+            let diff = window[0] - window[1];
+
+            if diff.abs() < MIN_LEVEL_DIFFERENCE || diff.abs() > MAX_LEVEL_DIFFERENCE {
+                is_safe = false;
+                break;
+            }
+
+            if diff > 0 {
+                increasing = true;
+            } else if diff < 0 {
+                decreasing = true;
+            }
+        }
+
+        // Check if we are strictly increasing or strictly decreasing
+        if is_safe && !(increasing && decreasing) {
+            total_safe_reports += 1;
+        }
+    }
+
+    Ok(total_safe_reports)
+}
+
 fn main() {
-    println!("Hello, world!");
+    match analyze_reports(FILE_PATH) {
+        Ok(total_safe_reports) => println!("Total safe reports: {}", total_safe_reports),
+        Err(e) => eprintln!("Error: {}", e),
+    }
 }
